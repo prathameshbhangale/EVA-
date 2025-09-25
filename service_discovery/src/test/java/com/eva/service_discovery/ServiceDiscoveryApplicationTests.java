89cfa56887bcf42f7f7465725e12eacd03e1cc1e
Prathamesh <114254813+prathameshbhangale@users.noreply.github.com>
2025-09-25 14:25:47 +0530

    eureka server

---
+++ service_discovery/src/test/java/com/eva/service_discovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eva.service_discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
